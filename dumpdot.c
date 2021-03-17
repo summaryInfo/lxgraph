a4bfc10722049686f9411a1c8b029034b359f6ad
Baskov Evgeniy <j-basevgser@yandex.ru>
2021-03-17 17:21:05 +0300

    Rudimental graphviz dump

--- dumpdot.c
+++ dumpdot.c
@@ -3,8 +3,24 @@
 #include "util.h"
 #include "callgraph.h"
 
+#include <stdio.h>
+
 void dump_dot(struct callgraph *cg, const char *destpath) {
-    (void)cg;
-    (void)destpath;
-    // TODO
+    FILE *dst = destpath ? fopen(destpath, "w") : stdout;
+    if (!dst) {
+        warn("Cannot open output file '%s'", destpath);
+        return;
+    }
+
+    // TODO Clustering
+    fputs("digraph \"callgraph\" {\n", dst);
+    for (size_t i = 0; i < cg->defs_size; i++) {
+        fprintf(dst, "\t%p[label=\"%s\"];\n", (void *)cg->defs[i].name, strtab_get(cg, cg->defs[i].name));
+    }
+    for (size_t i = 0; i < cg->calls_size; i++) {
+        fprintf(dst, "\t%p -> %p;\n", (void *)cg->calls[i].caller, (void *)cg->calls[i].callee);
+    }
+    fputs("}\n", dst);
+
+    if (dst != stdout) fclose(dst);
 }
