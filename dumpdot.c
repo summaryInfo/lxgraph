582253acb865389904a53ec3897293959d840797
Baskov Evgeniy <j-basevgser@yandex.ru>
2021-03-18 02:43:19 +0300

    Better removal of unused nodes

--- dumpdot.c
+++ dumpdot.c
@@ -5,20 +5,20 @@
 
 #include <stdio.h>
 
-void remove_unused(struct callgraph *cg) {
-    for (size_t i = 0; i < cg->defs_size; i++)
-        *literal_get_pdata(cg->defs[i]) = 0;
-    for (size_t i = 0; i < cg->calls_size; i++) {
-        (*literal_get_pdata(cg->calls[i].callee))++;
-        (*literal_get_pdata(cg->calls[i].caller))++;
-    }
-    literal *dst = cg->defs, *src = cg->defs;
-    literal *end = src + cg->defs_size;
-    for (; src < end; src++) {
-        if (*literal_get_pdata(*src))
-            *dst++ = *src;
-    }
-    cg->defs_size = dst - cg->defs;
+static int cmp_def(const void *a, const void *b) {
+    literal alit = *(literal *)a;
+    literal blit = *(literal *)b;
+    if (alit < blit) return -1;
+    else return (alit > blit);
+}
+
+static int cmp_def2(const void *a, const void *b) {
+    literal da = *(literal *)a, db = *(literal *)b;
+    literal fa = literal_get_file(da), fb = literal_get_file(db);
+    if (fa < fb) return -1;
+    else if (fa > fb) return 1;
+    if (da < db) return -1;
+    return (da > db);
 }
 
 static int cmp_call(const void *a, const void *b) {
@@ -30,8 +30,68 @@ static int cmp_call(const void *a, const void *b) {
     return (ia->callee > ib->callee);
 }
 
+static void dfs(literal root, struct invokation *calls, struct invokation *end) {
+    uint64_t *ptr = literal_get_pdata(root);
+    struct invokation *it = calls + (*ptr >> 16);
+    if (*ptr & 1) return;
+    *ptr |= 1;
+    for (; it < end && it->caller == root; it++)
+        dfs(it->callee, calls, end);
+}
+
+void remove_unused(struct callgraph *cg) {
+    {
+        // Clear marks and build
+        // associations between edges
+        // and nodes to be ables to dfs on O(n) time
+        size_t i = 0, j = 0;
+        while (j < cg->calls_size) {
+            while (i < cg->defs_size && cg->calls[j].caller != cg->defs[i])
+                *literal_get_pdata(cg->defs[i++]) = 0;
+            if (i == cg->defs_size) break;
+            *literal_get_pdata(cg->defs[i++]) = j++ << 16;
+            while (j < cg->calls_size &&
+                   cg->calls[j].caller == cg->calls[j - 1].caller) j++;
+        }
+        while (i < cg->defs_size)
+            *literal_get_pdata(cg->defs[i++]) = 0;
+    }
+
+    {
+        // Mark every function starting from roots
+
+        // TODO Make this configurable
+        const char *roots[] = {
+            "main(int, char **)",
+        };
+        for (size_t i = 0; i < sizeof roots/sizeof *roots; i++)
+            dfs(strtab_put(&cg->strtab, roots[i]), cg->calls, cg->calls + cg->calls_size);
+    }
+
+    {
+        // Remove unreachable edges
+        struct invokation *dst = cg->calls, *src = cg->calls + 1;
+        struct invokation *end = dst + cg->calls_size;
+        for (; src < end; src++) {
+            if (*literal_get_pdata(src->caller) & 1)
+                *dst++ = *src;
+        }
+        cg->calls_size = dst - cg->calls;
+    }
+
+    {
+        // Remove unreachable functions
+        literal *dst = cg->defs, *src = cg->defs + 1;
+        literal *end = dst + cg->defs_size;
+        for (; src < end; src++) {
+            if (*literal_get_pdata(*src) & 1)
+                *dst++ = *src;
+        }
+        cg->defs_size = dst - cg->defs;
+    }
+}
+
 void colapse_duplicates(struct callgraph *cg) {
-    qsort(cg->calls, cg->calls_size, sizeof cg->calls[0], cmp_call);
     struct invokation *dst = cg->calls, *src = cg->calls + 1;
     struct invokation *end = dst + cg->calls_size;
     for (; src < end; src++) {
@@ -49,8 +109,15 @@ void dump_dot(struct callgraph *cg, const char *destpath) {
         return;
     }
 
-    remove_unused(cg);
+    /* Sort by caller name */
+    qsort(cg->calls, cg->calls_size, sizeof cg->calls[0], cmp_call);
+    /* Sort by name */
+    qsort(cg->defs, cg->defs_size, sizeof cg->defs[0], cmp_def);
     colapse_duplicates(cg);
+    remove_unused(cg);
+
+    /* Sort by file */
+    qsort(cg->defs, cg->defs_size, sizeof cg->defs[0], cmp_def2);
 
     fputs("digraph \"callgraph\" {\n", dst);
     fprintf(dst, "\tlayout = \"circo\";\n");
