885566512fd860e7bbfc150545ec1490c206ba61
Baskov Evgeniy <j-basevgser@yandex.ru>
2021-03-18 22:55:40 +0300

    Better styling

--- dumpdot.c
+++ dumpdot.c
@@ -28,35 +28,36 @@ void dump_dot(struct callgraph *cg, const char *destpath) {
 
     debug("Writing graph to '%s'...", destpath ? destpath : "<stdout>");
 
-    // TODO make this configurable
-    const char *layout = "sfdp";
-
     fputs("digraph \"callgraph\" {\n", dst);
-    fprintf(dst, "\tlayout = \"%s\";\n", layout);
+    fprintf(dst, "\tlayout = \"%s\";\n", "fdp");
     fprintf(dst, "\tsmoothing = \"%s\";\n", "graph_dist");
-    fprintf(dst, "\tesep = \"+%u\";\n", 20);
-    fprintf(dst, "\toverlap = \"false\";\n");
+    fprintf(dst, "\tesep = \"+%u\";\n", 32);
+    fprintf(dst, "\toverlap = \"%s\";\n", "false");
+    fprintf(dst, "\tsplines = \"%s\";\n", "compound");
+    fprintf(dst, "\toutputorder = \"%s\";\n", "edgesfirst");
+    fprintf(dst, "\tnode[shape=\"%s\" style=\"%s\" color=\"%s\"]\n", "box", "filled", "white");
+
     literal old_file = (void *)-1, *firstdef = NULL, *edef = cg->defs + cg->defs_size;
 
     /* Print functions for each file */
     for (size_t i = 0; i < cg->defs_size; i++) {
         literal file = literal_get_file(cg->defs[i]);
         if (old_file != file) {
-            fprintf(dst, "\tsubgraph \"%s\" {\n", file ? literal_get_name(file) : "<external>");
+            fprintf(dst, "\tsubgraph \"cluster_%s\" {\n", file ? literal_get_name(file) : "<external>");
             fprintf(dst, "\t\tstyle = \"%s\";\n", "dotted,filled");
-            fprintf(dst, "\t\tcolor = \"%s\";\n", "black");
+            fprintf(dst, "\t\tcolor = \"%s\";\n", "lightgray");
             fprintf(dst, "\t\tlabel = \"%s\";\n", file ? literal_get_name(file) : "<external>");
             old_file = literal_get_file(cg->defs[i]);
             firstdef = cg->defs + i;
         }
-        fprintf(dst, "\t\tn%p[label=\"%s\" shape=\"box\"];\n", (void *)cg->defs[i], literal_get_name(cg->defs[i]));
+        fprintf(dst, "\t\tn%p[label=\"%s\"];\n", (void *)cg->defs[i], literal_get_name(cg->defs[i]));
         if (i < cg->defs_size && file != literal_get_file(cg->defs[i + 1])) {
             /* Print edges within current file */
             for (literal *def = firstdef; def < edef && literal_get_file(*def) == literal_get_file(*firstdef); def++) {
                 struct invokation *it = cg->calls + (*literal_get_pdata(*def) >> 16);
                 for (; it < cg->calls + cg->calls_size && it->caller == *def; it++)
                     if (literal_get_file(it->caller) == literal_get_file(it->callee))
-                        fprintf(dst, "\t\tn%p -> n%p;\n", (void *)it->caller, (void *)it->callee);
+                        fprintf(dst, "\t\tn%p -> n%p[style = \"setlinewidth(%f)\"];\n", (void *)it->caller, (void *)it->callee, pow(cg->calls[i].weight, 0.6));
             }
             fputs("\t}\n", dst);
         }
